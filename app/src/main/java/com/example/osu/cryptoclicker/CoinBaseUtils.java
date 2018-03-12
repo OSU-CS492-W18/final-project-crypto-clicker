cfe6afc874ad15778d6978191b4e608415e47796
ruefa <ruef.alex@gmail.com>
2018-03-12 14:06:51 -0700

    created class for coinbase api calls and included some public static strings for currencies

---
+++ app/src/main/java/com/example/osu/cryptoclicker/CoinBaseUtils.java
@@ -0,0 +1,11 @@
+package com.example.osu.cryptoclicker;
+
+/**
+ * Created by aruef on 3/12/2018.
+ */
+
+public class CoinBaseUtils {
+
+    public final static String USD = "usd";
+    public final static String BITCOIN = "bitcoin";
+}
