3397a4e954a317da3b597fc43f63bf6b605c02a8
ruefa <ruef.alex@gmail.com>
2018-03-12 13:11:08 -0700

    initialized DB in main activity

--- app/src/main/java/com/example/osu/cryptoclicker/MainActivity.java
+++ app/src/main/java/com/example/osu/cryptoclicker/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.osu.cryptoclicker;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SQLiteDatabase mDB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ClickerDBHelper dbHelper = new ClickerDBHelper(this);
+        mDB = dbHelper.getWritableDatabase();
     }
 
     public void goUpgrades(View v){
