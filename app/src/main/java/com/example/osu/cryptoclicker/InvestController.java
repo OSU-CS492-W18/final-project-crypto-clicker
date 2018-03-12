d388c8832d11d8e63c373fd10e6872fd9f2e08fb
ruefa <ruef.alex@gmail.com>
2018-03-12 12:36:30 -0700

    created activity for investing

---
+++ app/src/main/java/com/example/osu/cryptoclicker/InvestController.java
@@ -0,0 +1,13 @@
+package com.example.osu.cryptoclicker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InvestController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invest);
+    }
+}
