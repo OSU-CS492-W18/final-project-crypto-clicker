0eff7d78d9a7e77db8068dadfebe65e1d074f7ef
ruefa <ruef.alex@gmail.com>
2018-03-20 13:38:31 -0700

    created UpgradeRVAdapter with empty methods

---
+++ app/src/main/java/com/example/osu/cryptoclicker/UpgradeRVAdapter.java
@@ -0,0 +1,50 @@
+package com.example.osu.cryptoclicker;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Alex on 3/20/2018.
+ */
+
+public class UpgradeRVAdapter extends RecyclerView.Adapter<UpgradeRVAdapter.ViewHolder> {
+
+    ArrayList<String> mUpgrades;
+
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+
+    public UpgradeRVAdapter(){
+        
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
