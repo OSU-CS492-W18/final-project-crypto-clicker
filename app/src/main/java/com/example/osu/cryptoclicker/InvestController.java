a4161a53fab2f687cdb4d4ba67897aee23678720
algadhib <carxcar.2007@gmail.com>
2018-03-20 19:24:59 -0700

    created refresh menu item in investController

--- app/src/main/java/com/example/osu/cryptoclicker/InvestController.java
+++ app/src/main/java/com/example/osu/cryptoclicker/InvestController.java
@@ -8,6 +8,8 @@ import android.support.v4.app.LoaderManager.LoaderCallbacks;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -15,7 +17,6 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 
-//TODO: add menu item to refresh the page
 //TODO: present the current balance in usd and btc
 //TODO: do the logic of buying and selling btc
 
@@ -43,6 +44,23 @@ public class InvestController extends AppCompatActivity
         mLoadingError = (TextView)findViewById(R.id.tv_loading_error);
         mProgressBar = (ProgressBar)findViewById(R.id.pb_loading_bar_invest);
         getPricesData();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)   {
+        getMenuInflater().inflate(R.menu.invest_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)  {
+        switch (item.getItemId())   {
+            case R.id.refresh_price:
+                getPricesData();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void getPricesData() {
