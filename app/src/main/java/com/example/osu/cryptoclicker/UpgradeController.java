e96f797b21c159a31c389f92d24fa035c96d3409
ruefa <ruef.alex@gmail.com>
2018-03-21 21:03:01 -0700

    setup recycler view with dummy data

--- app/src/main/java/com/example/osu/cryptoclicker/UpgradeController.java
+++ app/src/main/java/com/example/osu/cryptoclicker/UpgradeController.java
@@ -2,12 +2,46 @@ package com.example.osu.cryptoclicker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
-public class UpgradeController extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class UpgradeController extends AppCompatActivity implements UpgradeRVAdapter.OnUpgradeClickListener {
+    private final static String TAG = "UpgradeController";
+
+    private UpgradeRVAdapter mUpgradeRVAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upgrade);
+
+        initUpgradesRV();
+    }
+
+    private void initUpgradesRV(){
+        RecyclerView upgradesRV = findViewById(R.id.rv_upgrades);
+        mUpgradeRVAdapter = new UpgradeRVAdapter(this);
+        upgradesRV.setAdapter(mUpgradeRVAdapter);
+        upgradesRV.setLayoutManager(new LinearLayoutManager(this));
+        upgradesRV.setHasFixedSize(true);
+        mUpgradeRVAdapter.updateData(testStrings(10));
+    }
+
+    private ArrayList<String> testStrings(int num){
+        ArrayList<String> ret = new ArrayList<>();
+
+        for(int i=1; i<=num; i++){
+            ret.add(String.valueOf(i));
+        }
+
+        return ret;
+    }
+
+    @Override
+    public void onUpgradeClick(String upgrade) {
+        Log.d(TAG, upgrade);
     }
 }
