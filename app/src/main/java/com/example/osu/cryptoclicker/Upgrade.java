610061f9bcea095f822971213de6e5c85f048ccf
ruefa <ruef.alex@gmail.com>
2018-03-22 17:26:51 -0700

    created Upgrade class with constructor

---
+++ app/src/main/java/com/example/osu/cryptoclicker/Upgrade.java
@@ -0,0 +1,18 @@
+package com.example.osu.cryptoclicker;
+
+/**
+ * Created by Alex on 3/22/2018.
+ */
+
+public class Upgrade {
+
+    private String mName;
+    private double mAmount;
+    private double mCost;
+
+    public Upgrade(String name, double amount, double cost){
+        mName = name;
+        mAmount = amount;
+        mCost = cost;
+    }
+}
