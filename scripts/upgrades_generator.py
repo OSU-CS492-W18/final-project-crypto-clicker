6a506f9a75d11ba7543637d66e0a5a7d6f581988
ruefa <ruef.alex@gmail.com>
2018-03-22 18:30:16 -0700

    created a python script to generate upgrade xml files

---
+++ scripts/upgrades_generator.py
@@ -0,0 +1,32 @@
+import random
+import xml.etree.ElementTree as ET
+
+#collect names from file
+names = []
+with open("upgrade_names.txt", 'r') as f:
+    for line in f:
+        names.append(line[:-1])
+
+#shuffle name order cause why not
+random.shuffle(names)
+
+amount = 50
+cost = 5.0
+root = ET.Element('upgrades')
+#with open("upgrades.xml", 'w+') as f:
+for name in names:
+    upgrade = ET.SubElement(root, 'upgrade')
+
+    nameTag = ET.SubElement(upgrade, 'name')
+    nameTag.text = name
+
+    amountTag = ET.SubElement(upgrade, 'amount')
+    amountTag.text = str(amount)
+    amount = amount + 25
+
+    costTag = ET.SubElement(upgrade, 'cost')
+    costTag.text = str(cost)
+    cost = cost*(1 + float(amount)/10.0)
+
+tree = ET.ElementTree(root)
+tree.write("upgrades.xml")
